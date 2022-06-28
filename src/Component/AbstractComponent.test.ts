01d27d6fd57d9fc856f4e9b003618b3ad583c373
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2022-06-27 22:07:38 -0400

    init commit

---
+++ src/Component/AbstractComponent.test.ts
@@ -0,0 +1,12 @@
+import { Entity } from '..';
+import { Component } from './AbstractComponent';
+
+it('should contain a reference to its Entity', () => {
+	class TestEntity extends Entity {}
+	class TestComponent extends Component {}
+	const entity = new TestEntity();
+
+	const component = new TestComponent(entity);
+
+	expect(component.entity).toBe(entity);
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { App } from './App/App';
+
+export * from './Entity/AbstractEntity';
+export * from './Component/AbstractComponent';
+export * from './System/AbstractSystem';
+
+export default App;
